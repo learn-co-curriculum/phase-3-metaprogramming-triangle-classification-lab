558fe8b43258a0d8a94fb5af2b9e52afa88805ff
ihollander <ianholla@gmail.com>
2021-07-12 13:56:06 -0400

    Refactor solution

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,6 @@
 class Triangle
   attr_reader :a, :b, :c
+  
   def initialize(a, b, c)
     @a = a
     @b = b
@@ -17,12 +18,16 @@ class Triangle
     end
   end
 
+  def sides_greater_than_zero?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
   def validate_triangle
-    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
-    [a, b, c].each do |side|
-      real_triangle << false if side <= 0 
-    raise TriangleError if real_triangle.include?(false)
-    end
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
   end
 
   class TriangleError < StandardError
